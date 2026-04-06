b1183c4e971aefb29ec6a58f46c6eea48207fa0d
Aborisade <bobleeswaggs123@gmail.com>
2026-04-06 16:23:38 +0100

    finally fixed the map and styling error

--- components/MapClient.tsx
+++ components/MapClient.tsx
@@ -58,11 +58,13 @@ export default function MapClient({ datasets, submissions, onPointClick, adminVi
       const map = L.map(containerRef.current, {
         center:           [2, 22],
         zoom:             4,
-        zoomControl:      true,
+        zoomControl:      false,
         attributionControl: true,
         minZoom:          2,
         maxZoom:          17,
       });
+
+      L.control.zoom({ position: "topright" }).addTo(map);
 
       L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a> | African MSDI',
