dacbc4dbe94598d2fcb1899e766c421756795de2
Aborisade <bobleeswaggs123@gmail.com>
2026-04-08 15:45:57 +0100

    updated the next

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  const nextConfig = {
+  turbopack: {},
 };
 
 export default nextConfig;
